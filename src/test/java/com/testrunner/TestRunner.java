5cc8eeb8b6b3969489fc24e7e00db9d7666629de
archana2402 <archana2402@gmail.com>
2022-08-23 23:55:57 +0530

    Archana - Pushing the code of Cucumber

---
+++ src/test/java/com/testrunner/TestRunner.java
@@ -0,0 +1,21 @@
+package com.testrunner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions
+(
+		features= {"Feature"}, // folder name of feature file
+		glue= {"com.itview.stepdefination"}, // package name of step def class
+		plugin= {"pretty","html:target/cucumber-report.html"}, // report
+		monochrome = true,
+		tags = "@DataDriven"
+		
+		
+	)
+
+public class TestRunner {
+}
